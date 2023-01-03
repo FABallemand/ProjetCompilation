975fb7614d4a176a311b9b8c7eb2c514cce3b50b
slebot <samuel.lebot@etu.unistra.fr>
2023-01-03 18:09:58 +0100

    #6 Amélioration du mips, ajout de mult, div, mod, concat, echo

--- mips/string.asm
+++ mips/string.asm
@@ -3,25 +3,35 @@
 .data
 
 ##########TEST####################
-string1: .asciiz "-"
-string2: .asciiz "9123456"
-string3: .asciiz "+5764"
+string1: .asciiz "Hello"
+string2: .asciiz "-17"
+string3: .asciiz "World"
 string4: .asciiz ""
-string5: .asciiz "-05677"
+string5: .asciiz "3"
 ##########TEST####################
 
 
 error_string_not_an_int: .asciiz "erreur : la chaine que vous essayez de convertir n'est pas un entier."
 error_overflow: .asciiz "erreur : overflow detecté."
 int_to_str: .space 512
-
+one_space: .asciiz " "
 .text
 
 
 main:
-	la $a0, string2
-	la $a1, string5
-	jal sub_string
+	addi $sp, $sp, -20
+	la $t0, string1
+	la $t1, string3
+	la $t2, string2
+	la $t3, string3
+	la $t4, string5
+	sw $t0, 0($sp)          
+	sw $t1, 4($sp)
+	sw $t2, 8($sp)          
+	sw $t3, 12($sp)
+	sw $t4, 16($sp)
+	li $a0, 5
+	jal echo_string
 	move $a0, $v0
 	li $v0, 4
 	syscall
@@ -178,6 +188,8 @@ csto_loop:
 csto_exit:
 	mult $t4, $t5 #on rajoute le signe 
 	mflo $v0
+	mthi $zero
+	mtlo $zero
 	lw   $ra, 40($sp)
 	lw   $t9, 36($sp)           # restore $t9 value before function was called
 	lw   $t8, 32($sp)           # restore $t8 value before function was called
@@ -273,7 +285,9 @@ j    pop_digits           # and loop
 
 next2:
 sb  $zero, ($a1)          # *str = 0 (end of string marker)
-
+	
+	mthi $zero
+	mtlo $zero
 	lw   $ra, 40($sp)
 	lw   $t9, 36($sp)           # restore $t9 value before function was called
 	lw   $t8, 32($sp)           # restore $t8 value before function was called
@@ -440,3 +454,374 @@ sub_string_error_not_int:
         syscall
 
 
+# PAS DE VERIFICATION D'OVERFLOW
+# entré : $a0 -> chaine 1
+#	  $a1 -> chaine 2
+# sorti:  $v0 -> chaine nouvellement malloc contenant le reslutat de l'opération
+mult_string:
+
+	addi $sp, $sp, -44 # to avoid headaches save $t- registers used in this procedure on stack
+	sw   $t0, 0($sp)          
+	sw   $t1, 4($sp)
+	sw   $t2, 8($sp)          
+	sw   $t3, 12($sp)  
+	sw   $t4, 16($sp)          
+	sw   $t5, 20($sp)  
+	sw   $t6, 24($sp)          
+	sw   $t7, 28($sp)  
+	sw   $t8, 32($sp)          
+	sw   $t9, 36($sp)        
+	sw   $ra, 40($sp)
+
+move $t0, $a0 # faire une copie d'arg 1
+move $t1, $a1 # faire une copie d'arg 2
+jal is_integer # on test si l'arg 1 est bien un entier 
+beqz $v0, mult_string_error_not_int
+jal convert_string_to_int #on converti l'entier en nombre
+move $t2, $v0
+move $a0, $a1
+jal is_integer # on test si l'arg 1 est bien un entier 
+beqz $v0, mult_string_error_not_int
+jal convert_string_to_int #on converti l'entier en nombre
+move $t3, $v0
+
+li $t4, 16 # le nombre maximal sur 32 bit signé est 2 147 483 647 (donc 10 char + le sentinel). j'alloue 16 pour plus de sécurité (a chnager durant l'optimisation).
+
+mult_next_1:
+move $a0, $t4
+li $v0, 9
+syscall # allocation de la memoire necessaire
+move $a1, $v0 # on met v0 dans a0 pour pouvoir exec le convertisseru un peu plus tard
+mult $t2 , $t3 # on additionne les deux opérandes
+mflo $a0 # on met le resultat de l'opération dans a0 pour le transformer en string
+mfhi $t9 # check si il y a un un overflow
+beqz $t9, suite_mult_l
+beq $t9, -1, suite_mult_l # il y a un probleme ici (certain overflow nous echappe dans les negatifs) mais je suis pas ssez malin pour le fix
+mthi $zero
+j mult_string_err
+suite_mult_l:
+jal convert_int_to_string
+j mult_string_exit
+
+mult_string_exit:
+	
+	lw   $ra, 40($sp)
+	lw   $t9, 36($sp)           # restore $t9 value before function was called
+	lw   $t8, 32($sp)           # restore $t8 value before function was called
+	lw   $t7, 28($sp)           # restore $t7 value before function was called
+	lw   $t6, 24($sp)           # restore $t6 value before function was called
+	lw   $t5, 20($sp)           # restore $t5 value before function was called
+	lw   $t4, 16($sp)           # restore $t4 value before function was called
+	lw   $t3, 12($sp)           # restore $t3 value before function was called
+	lw   $t2, 8($sp)           # restore $t2 value before function was called
+	lw   $t1, 4($sp)           # restore $t1 value before function was called
+	lw   $t0, 0($sp)           # restore $t0 value before function was called
+	addi $sp, $sp, 44  
+	jr $ra
+	
+mult_string_error_not_int:
+	li $v0, 4
+	la $a0, error_string_not_an_int
+        syscall
+        li $v0, 10
+        syscall
+
+mult_string_err:
+	li $v0, 4
+	la $a0, error_overflow
+        syscall
+        li $v0, 10
+        syscall	
+
+        
+# PAS DE VERIFICATION D'OVERFLOW
+# entré : $a0 -> chaine 1
+#	  $a1 -> chaine 2
+# sorti:  $v0 -> chaine nouvellement malloc contenant le reslutat de l'opération
+div_string:
+
+	addi $sp, $sp, -44 # to avoid headaches save $t- registers used in this procedure on stack
+	sw   $t0, 0($sp)          
+	sw   $t1, 4($sp)
+	sw   $t2, 8($sp)          
+	sw   $t3, 12($sp)  
+	sw   $t4, 16($sp)          
+	sw   $t5, 20($sp)  
+	sw   $t6, 24($sp)          
+	sw   $t7, 28($sp)  
+	sw   $t8, 32($sp)          
+	sw   $t9, 36($sp)        
+	sw   $ra, 40($sp)
+
+move $t0, $a0 # faire une copie d'arg 1
+move $t1, $a1 # faire une copie d'arg 2
+jal is_integer # on test si l'arg 1 est bien un entier 
+beqz $v0, div_string_error_not_int
+jal convert_string_to_int #on converti l'entier en nombre
+move $t2, $v0
+move $a0, $a1
+jal is_integer # on test si l'arg 1 est bien un entier 
+beqz $v0, div_string_error_not_int
+jal convert_string_to_int #on converti l'entier en nombre
+move $t3, $v0
+
+li $t4, 16 # le nombre maximal sur 32 bit signé est 2 147 483 647 (donc 10 char + le sentinel). j'alloue 16 pour plus de sécurité (a chnager durant l'optimisation).
+
+div_next_1:
+move $a0, $t4
+li $v0, 9
+syscall # allocation de la memoire necessaire
+move $a1, $v0 # on met v0 dans a0 pour pouvoir exec le convertisseru un peu plus tard
+div $t2 , $t3 # on additionne les deux opérandes
+mflo $a0 # on met le resultat de l'opération dans a0 pour le transformer en string
+mthi $zero
+mtlo $zero
+jal convert_int_to_string
+j div_string_exit
+
+div_string_exit:
+	
+	lw   $ra, 40($sp)
+	lw   $t9, 36($sp)           # restore $t9 value before function was called
+	lw   $t8, 32($sp)           # restore $t8 value before function was called
+	lw   $t7, 28($sp)           # restore $t7 value before function was called
+	lw   $t6, 24($sp)           # restore $t6 value before function was called
+	lw   $t5, 20($sp)           # restore $t5 value before function was called
+	lw   $t4, 16($sp)           # restore $t4 value before function was called
+	lw   $t3, 12($sp)           # restore $t3 value before function was called
+	lw   $t2, 8($sp)           # restore $t2 value before function was called
+	lw   $t1, 4($sp)           # restore $t1 value before function was called
+	lw   $t0, 0($sp)           # restore $t0 value before function was called
+	addi $sp, $sp, 44  
+	jr $ra
+	
+div_string_error_not_int:
+	li $v0, 4
+	la $a0, error_string_not_an_int
+        syscall
+        li $v0, 10
+        syscall
+
+
+
+
+# PAS DE VERIFICATION D'OVERFLOW
+# entré : $a0 -> chaine 1
+#	  $a1 -> chaine 2
+# sorti:  $v0 -> chaine nouvellement malloc contenant le reslutat de l'opération
+mod_string:
+
+	addi $sp, $sp, -44 # to avoid headaches save $t- registers used in this procedure on stack
+	sw   $t0, 0($sp)          
+	sw   $t1, 4($sp)
+	sw   $t2, 8($sp)          
+	sw   $t3, 12($sp)  
+	sw   $t4, 16($sp)          
+	sw   $t5, 20($sp)  
+	sw   $t6, 24($sp)          
+	sw   $t7, 28($sp)  
+	sw   $t8, 32($sp)          
+	sw   $t9, 36($sp)        
+	sw   $ra, 40($sp)
+
+move $t0, $a0 # faire une copie d'arg 1
+move $t1, $a1 # faire une copie d'arg 2
+jal is_integer # on test si l'arg 1 est bien un entier 
+beqz $v0, mod_string_error_not_int
+jal convert_string_to_int #on converti l'entier en nombre
+move $t2, $v0
+move $a0, $a1
+jal is_integer # on test si l'arg 1 est bien un entier 
+beqz $v0, mod_string_error_not_int
+jal convert_string_to_int #on converti l'entier en nombre
+move $t3, $v0
+
+li $t4, 16 # le nombre maximal sur 32 bit signé est 2 147 483 647 (donc 10 char + le sentinel). j'alloue 16 pour plus de sécurité (a chnager durant l'optimisation).
+
+mod_next_1:
+move $a0, $t4
+li $v0, 9
+syscall # allocation de la memoire necessaire
+move $a1, $v0 # on met v0 dans a0 pour pouvoir exec le convertisseru un peu plus tard
+div $t2 , $t3 # on additionne les deux opérandes
+mfhi $a0 # on met le resultat de l'opération dans a0 pour le transformer en string
+mthi $zero
+mtlo $zero
+jal convert_int_to_string
+j mod_string_exit
+
+mod_string_exit:
+	
+	lw   $ra, 40($sp)
+	lw   $t9, 36($sp)           # restore $t9 value before function was called
+	lw   $t8, 32($sp)           # restore $t8 value before function was called
+	lw   $t7, 28($sp)           # restore $t7 value before function was called
+	lw   $t6, 24($sp)           # restore $t6 value before function was called
+	lw   $t5, 20($sp)           # restore $t5 value before function was called
+	lw   $t4, 16($sp)           # restore $t4 value before function was called
+	lw   $t3, 12($sp)           # restore $t3 value before function was called
+	lw   $t2, 8($sp)           # restore $t2 value before function was called
+	lw   $t1, 4($sp)           # restore $t1 value before function was called
+	lw   $t0, 0($sp)           # restore $t0 value before function was called
+	addi $sp, $sp, 44  
+	jr $ra
+	
+mod_string_error_not_int:
+	li $v0, 4
+	la $a0, error_string_not_an_int
+        syscall
+        li $v0, 10
+        syscall
+       
+       
+
+        
+# PAS DE VERIFICATION D'OVERFLOW
+# entré : $a0 -> chaine 1
+#	  $a1 -> chaine 2
+# sorti:  $v0 -> chaine nouvellement malloc contenant la concatenation
+concat_string:
+
+	addi $sp, $sp, -44 # to avoid headaches save $t- registers used in this procedure on stack
+	sw   $t0, 0($sp)          
+	sw   $t1, 4($sp)
+	sw   $t2, 8($sp)          
+	sw   $t3, 12($sp)  
+	sw   $t4, 16($sp)          
+	sw   $t5, 20($sp)  
+	sw   $t6, 24($sp)          
+	sw   $t7, 28($sp)  
+	sw   $t8, 32($sp)          
+	sw   $t9, 36($sp)        
+	sw   $ra, 40($sp)
+
+move $t0, $a0 # faire une copie d'arg 1
+move $t1, $a1 # faire une copie d'arg 2
+jal count_char # choper la taille de la chaine 1
+move $t2, $v0 # on enregistre la taille dans une var temp
+move $a0, $a1 # on place la chaine 2 dans l'argument 0 pour comptage
+jal count_char # choper la taille de la chaine 1
+move $t3, $v0 # on enregistre la taille dans une var temp
+
+add $t4, $t2, $t3 # la taille de la chaine resultante est egale à la somme des tailles des chaines precedentes (+1 pour le char sentinel)
+add $t4, $t4, 1 #pour le char sentinel
+
+move $a0, $t4
+li $v0, 9
+syscall # allocation de la memoire necessaire
+move $t5, $v0 # on enregristre l'adresse de la zone memoire quelque part pour la renvoyer a la fin
+move $t6, $v0 # on enregristre l'adresse de la zone memoire quelque part pour la modifier
+
+loop_concat_1:
+lb $t7, 0($t0) # j'enregistre temporairement le char en debut de ma chaine
+beqz $t7, loop_concat_2 # si j'ai fini de copier la liste 1 je passe a la deuxième
+add $t0, $t0, 1 # je deplace ma tête de lecture sur le prochain char de la chaine 1
+sb $t7, 0($t6) # j'enregistre mon char dans la chaine final
+add $t6, $t6, 1 # j'avance de 1 dans la chaine final 
+j loop_concat_1
+
+loop_concat_2:
+lb $t7, 0($t1) # j'enregistre temporairement le char en debut de ma chaine
+beqz $t7, concat_string_exit # si j'ai fini de copier la liste 1 je passe a la deuxième
+add $t1, $t1, 1 # je deplace ma tête de lecture sur le prochain char de la chaine 1
+sb $t7, 0($t6) # j'enregistre mon char dans la chaine final
+add $t6, $t6, 1 # j'avance de 1 dans la chaine final
+j loop_concat_2
+
+
+concat_string_exit:
+	sb $zero, 0($t6)
+	move $v0, $t5
+	lw   $ra, 40($sp)
+	lw   $t9, 36($sp)           # restore $t9 value before function was called
+	lw   $t8, 32($sp)           # restore $t8 value before function was called
+	lw   $t7, 28($sp)           # restore $t7 value before function was called
+	lw   $t6, 24($sp)           # restore $t6 value before function was called
+	lw   $t5, 20($sp)           # restore $t5 value before function was called
+	lw   $t4, 16($sp)           # restore $t4 value before function was called
+	lw   $t3, 12($sp)           # restore $t3 value before function was called
+	lw   $t2, 8($sp)           # restore $t2 value before function was called
+	lw   $t1, 4($sp)           # restore $t1 value before function was called
+	lw   $t0, 0($sp)           # restore $t0 value before function was called
+	addi $sp, $sp, 44  
+	jr $ra
+
+
+# entré : $a0 -> nombre de chaine a afficher (on considère que leurs adresses ont été rentré dans l'ordre dans la pile
+# sorti:  rien
+# ATTENTION : On reduit la taille de la pile a l'intérieur du echo !!!! ça permet de s'épargner un quad !!!! à la fin de cette fonction le pointeur de pile est plus grand de a0*4 
+echo_string:
+	addi $sp, $sp, -44 # to avoid headaches save $t- registers used in this procedure on stack
+	sw   $t0, 0($sp)          
+	sw   $t1, 4($sp)
+	sw   $t2, 8($sp)          
+	sw   $t3, 12($sp)  
+	sw   $t4, 16($sp)          
+	sw   $t5, 20($sp)  
+	sw   $t6, 24($sp)          
+	sw   $t7, 28($sp)  
+	sw   $t8, 32($sp)          
+	sw   $t9, 36($sp)        
+	sw   $ra, 40($sp)
+	
+	move $t0, $a0
+	li $t1, 40 # offset initial des arguments dans la pile -> normalement 44 mais vue que au minimum on a 1 argument ça fait 40+4*1 donc 44
+	beqz $t0, echo_string_exit
+	# on commance par print le print la premiere chaine (car si elle est seuk on ne rajoutera pas d'espace)
+	mul $t2, $t0, 4 # je multiplie le nombre d'argmuent restant par la taille d'un mot (4)
+	add $t3, $t2, $t1 # j'additionne l'offsey aisni obtenu avec l'offset inititial
+	add $t3, $t3, $sp # je calcule l'adresse réel de l'élément grace au stack pointer
+	lw $a0, 0($t3) # je deplace cette adresse dans a0
+	li $v0, 4
+	syscall
+	sub $t0, $t0, 1 # je decremente le nombre de chaine a afficher
+echo_string_loop:
+	beqz $t0, echo_string_exit
+	la $a0, one_space
+	li $v0, 4
+	syscall
+	mul $t2, $t0, 4 # je multiplie le nombre d'argmuent restant par la taille d'un mot (4)
+	add $t3, $t2, $t1 # j'additionne l'offsey aisni obtenu avec l'offset inititial
+	add $t3, $t3, $sp # je calcule l'adresse réel de l'élément grace au stack pointer
+	lw $a0, 0($t3) # je deplace cette adresse dans a0
+	li $v0, 4
+	syscall
+	sub $t0, $t0, 1 # je decremente le nombre de chaine a afficher
+	j echo_string_loop
+	
+echo_string_exit:
+
+	lw   $ra, 40($sp)
+	lw   $t9, 36($sp)           # restore $t9 value before function was called
+	lw   $t8, 32($sp)           # restore $t8 value before function was called
+	lw   $t7, 28($sp)           # restore $t7 value before function was called
+	lw   $t6, 24($sp)           # restore $t6 value before function was called
+	lw   $t5, 20($sp)           # restore $t5 value before function was called
+	lw   $t4, 16($sp)           # restore $t4 value before function was called
+	lw   $t3, 12($sp)           # restore $t3 value before function was called
+	lw   $t2, 8($sp)           # restore $t2 value before function was called
+	lw   $t1, 4($sp)           # restore $t1 value before function was called
+	lw   $t0, 0($sp)           # restore $t0 value before function was called
+	addi $sp, $sp, 44  
+	mul $t0, $a0, 4
+	mtlo $zero
+	addi $sp, $sp, 20 # On reduit la taille de la pile a l'intérieur du echo !!!! ça permet de s'épargner un quad!!!! a la fin de cette fonction le pointeur de pile est plus grand de a0*4 
+	jr $ra
+	
+# entré : $a0 -> l'adresse de l'espace memoire ou mettre l'adresse de la chaine nouvellement lu (et malloc)
+# sorti:  rien
+read_string:
+	addi $sp, $sp, -44 # to avoid headaches save $t- registers used in this procedure on stack
+	sw   $t0, 0($sp)          
+	sw   $t1, 4($sp)
+	sw   $t2, 8($sp)          
+	sw   $t3, 12($sp)  
+	sw   $t4, 16($sp)          
+	sw   $t5, 20($sp)  
+	sw   $t6, 24($sp)          
+	sw   $t7, 28($sp)  
+	sw   $t8, 32($sp)          
+	sw   $t9, 36($sp)        
+	sw   $ra, 40($sp)
+
+	
